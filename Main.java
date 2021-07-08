6167bdb957ea276727c17f1e6367f42e2eef2174
rodrigomb13 <60118286+rodrigomb13@users.noreply.github.com>
2021-07-08 10:59:02 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+package orientada_aspects;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		TargetClass tc = new TargetClass();
+		tc.run();
+
+	}
+
+}
---
+++ TargetClass.java
@@ -0,0 +1,8 @@
+package orientada_aspects;
+
+public class TargetClass {
+	
+	public void run() {
+		System.out.println("TargetClass Running!");
+	}
+}
